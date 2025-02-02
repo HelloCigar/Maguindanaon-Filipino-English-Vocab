1b11d256bad245df0cc23b729a1126cc0fc64e09
HelloCigar <100703625+HelloCigar@users.noreply.github.com>
2025-02-02 13:58:22 +0800

    add meta for SEO

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
 <script setup lang="ts">
 import { Toaster } from '@/components/ui/sonner'
+useSeoMeta({
+  title: 'Maguindanaon-Filipino-English Vocabulary',
+  ogTitle: 'Maguindanaon-Filipino-English Vocabulary',
+  description: 'Maguindanaon vocabulary based on the book "Vocabulary: Magindanawn-Pilipino-English; Pilipino-English-Magindanawn; English-Pilipino-Magindanawn." by Fleischman et. al',
+  ogDescription: 'Maguindanaon vocabulary based on the book "Vocabulary: Magindanawn-Pilipino-English; Pilipino-English-Magindanawn; English-Pilipino-Magindanawn." by Fleischman et. al',
+  robots: 'index, follow',
+  ogType: 'website',
+  ogUrl: 'https://https://maguindanaon-filipino-english-vocab.netlify.app/',
+  charset: 'utf-8',
+})
+
 </script>
 
 <template>
